752a7ae75d9ab20925ab053408d370dd3d555b0d
RXDCODX <maxxxes73rus@gmail.com>
2026-06-12 16:05:51 +0400

    refactor: remove application shutdown functionality from HealthController
    Simplify the HealthController by removing the shutdown endpoint and IHostApplicationLifetime dependency. The shutdown functionality is being removed as it's no longer needed in the current application design.

--- Controllers/HealthController.cs
+++ Controllers/HealthController.cs
@@ -4,18 +4,11 @@ namespace MARS.AudioController.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class HealthController(IHostApplicationLifetime lifetime) : ControllerBase
+public class HealthController : ControllerBase
 {
     [HttpGet]
     public IActionResult Get()
     {
         return Ok(new { success = true, message = "OK" });
-    }
-
-    [HttpPost("shutdown")]
-    public IActionResult Shutdown()
-    {
-        lifetime.StopApplication();
-        return Ok(new { success = true, message = "AudioController shutting down" });
     }
 }
